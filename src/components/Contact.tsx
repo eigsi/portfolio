ce5e5f535c6853fc2a529b2c5f729a466e76777b
eigsi <126054825+eigsi@users.noreply.github.com>
2025-01-29 15:37:17 +0100

    update contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,42 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
 import FloatingLabelInput from './FloatingLabel';
-import '/src/assets/css/Contact.css'
+import '/src/assets/css/Contact.css';
 
 function Contact() {
+    const formRef = useRef<HTMLDivElement>(null);
 
+    useEffect(() => {
+        if (formRef.current) {
+            gsap.fromTo(
+                formRef.current,
+                { opacity: 0, y: 200, scale: 1.5 },
+                {
+                    opacity: 1,
+                    y: 0,
+                    scale: 1,
+                    duration: 1.2,
+                    ease: "power2.out",
+                    scrollTrigger: {
+                        trigger: formRef.current,
+                        start: "top 100%",
+                        toggleActions: "play"
+                    }
+                }
+            );
+        }
+    }, []);
 
     return (
-        <section className="contact" id='contact'>
+        <section className="contact" id="contact" ref={formRef}>
             <h2>Contact</h2>
             <form action="https://formspree.io/f/{ton_form_id}" method="POST">
                 <FloatingLabelInput label="Your Email" name="email" type="email" />
                 <FloatingLabelInput label="Your Message" name="message" isTextarea={true} />
-                <button className='sendButton'>Send</button>
+                <button className="sendButton">Send</button>
             </form>
         </section>
     );
-};
+}
 
 export default Contact;
